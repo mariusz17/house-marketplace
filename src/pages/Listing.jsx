ddae9dfb69a99b6708498ad5bef1d0d561dbd146
mariusz17 <qbajt.mariusz.kubat@gmail.com>
2022-07-05 17:19:08 +0200

    add leaflet map to the listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,8 +1,9 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { doc, getDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { db } from "../firebase.config";
-import { useEffect, useState } from "react";
-import { useNavigate, useParams, Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import shareIcon from "../assets/svg/shareIcon.svg";
 import Spinner from "../components/Spinner";
@@ -94,7 +95,27 @@ const Listing = () => {
         </ul>
         <p className="listingLocationTitle">Location</p>
 
-        {/* MAP */}
+        <div className="leafletContainer">
+          <MapContainer
+            style={{
+              height: "100%",
+              width: "100%",
+            }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
